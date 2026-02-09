979592f2306bc13f9502680dfac5516f96db07d3
Dhanu297 <dhanashree.thakur@gmail.com>
2026-02-08 23:11:51 -0700

    vercel

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const path = require("path");
+
 const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use("/users", require("./src/routes/userRoutes"));
 app.use("/expenses", require("./src/routes/expenseRoutes"));
